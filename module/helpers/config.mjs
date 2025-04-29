7b1af7704ada679176cbd2a58566a4233f83aa0f
smrflynn <sflynn314@gmail.com>
2025-04-28 22:37:23 -0500

    intial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const WRATH_OF_DAVOKAR = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+WRATH_OF_DAVOKAR.abilities = {
+  str: 'WRATH_OF_DAVOKAR.Ability.Str.long',
+  dex: 'WRATH_OF_DAVOKAR.Ability.Dex.long',
+  con: 'WRATH_OF_DAVOKAR.Ability.Con.long',
+  int: 'WRATH_OF_DAVOKAR.Ability.Int.long',
+  wis: 'WRATH_OF_DAVOKAR.Ability.Wis.long',
+  cha: 'WRATH_OF_DAVOKAR.Ability.Cha.long',
+};
+
+WRATH_OF_DAVOKAR.abilityAbbreviations = {
+  str: 'WRATH_OF_DAVOKAR.Ability.Str.abbr',
+  dex: 'WRATH_OF_DAVOKAR.Ability.Dex.abbr',
+  con: 'WRATH_OF_DAVOKAR.Ability.Con.abbr',
+  int: 'WRATH_OF_DAVOKAR.Ability.Int.abbr',
+  wis: 'WRATH_OF_DAVOKAR.Ability.Wis.abbr',
+  cha: 'WRATH_OF_DAVOKAR.Ability.Cha.abbr',
+};
